390ba877865568b1679c530be6ca430699eeb8ae
Andrew Bonich <39278382+ajbonich@users.noreply.github.com>
2021-01-07 08:12:49 -0700

    Added average data percentile functionality.  Moved to Serverless from Zappa (#2)
    * Added quantile returns to daily average data.
    
    * Changed default flows for runnablePercentages endpoint
    
    * Renamed the mean column to average
    
    * Changed column to percent from mean.
    
    * Changed better default data.
    Modified returned quantile80 to be the difference between 80 and 20 to work with a stacked graph.
    
    * Added yml for Serverless to use to deploy to AWS.
    
    * Changed service name to python-api.
    
    * Added node_modules and .serverless to gitignore.
    initial package.json commit.
    Reverted requirements.txt to barebones.
    
    * Removed flask.
    
    * Removed comma to fix build error.
    
    * Added requests and datetime to requirements.txt.
    
    * Changed spacing and added numpy
    
    * Removed Flask stuff from usgsConttoller.py
    Split out imports.
    
    * Replaces requests with event for aws.
    
    * Added the numpy layer to be able to use numpy.
    
    * Fixed spacing.
    
    * Added pandas layer
    
    * Rename testFIle.json to testFile.json
    
    * added print statement
    
    * Changed function return to a http response.
    
    * Changed arns to be used by all functions.
    
    * Added serverless-layers
    
    * Added deploymentBucket
    
    * Added additional  functions
    
    * Named custom bucket
    
    * Renamed bucket
    
    * Made my own bucket
    
    * Removed bucket
    
    * Added serverless-deployment-bucket
    
    * Added serverless-deployment-bucket.
    
    * Added serverless-deployment-bucket to package.json
    
    * Updated package-lock.json
    
    * Fixed function name
    
    * Fixed duplicate numpy arn with pandas arn
    
    * Using one arn
    
    * Changed static arn to a string rather than a list.
    
    * Added back pandas arn
    
    * Added event and object to be abke to pass parameters.
    
    * Added AWS friendly requests import(urllib3)
    
    * Updated correct quantile labels
    
    * Removed old files
    
    * Added cors to all endpoints.
    
    * Removed s to fix typo issues.
    
    * Added cors headers to responses.
    
    * Added additional access headers
    
    * Added log to see passed params
    
    * Added addional log
    
    * Changed param processing
    
    * Removed /
    
    * Single param
    
    * More changes
    
    * Changed spacing
    
    * Added a slash
    
    * Added multiple params and logging
    
    * Updated spacing
    
    * Updated spacing
    
    * Added more print statements
    
    * Changed loggin
    
    * Fixed (
    
    * Add lots of print statements.
    
    * More logs x2
    
    * More log x3
    
    * More logs
    
    * Removed extra stuff to see if query params work in Runnable Percentage
    
    * Added check to see if we get min and max flow too
    
    * checking error handling of not passing a value
    
    * Potentially clean averageData endpoint
    
    * 2nd clean try dailyDataAverage
    
    * Clean runnablepercentages
    
    * Casted query params to ints for min and max flow.
    
    * Changed zero values to nan so they won't be included in mean.
    
    * Added a fix to handle nan values when calculating runnable percentage
    
    * Corrected variable name
    
    * Fixed errors.
    
    * Fixed leap day issue.
    
    * Fixed count axis to day of the year rather than year.
    
    * Correctee runnable percentages value label from 0.
    
    * Changed percent runnable label to percent

--- usgsController.py
+++ usgsController.py
@@ -1,102 +1,177 @@
-import datetime, json, requests
+import datetime
+import json
 import numpy as np
 import pandas as pd
-from flask import Flask
-from flask import request
-from flask import jsonify
+import urllib3
+
+# Uncomment if graphs need to be tested locally for some reason
 # import matplotlib.dates as mdates
 # import matplotlib.pyplot as plt
-#from pandas.plotting import register_matplotlib_converters
+# from pandas.plotting import register_matplotlib_converters
 
-#TODO: make a call to get the period data is available
+# TODO: make a call to get the period data is available
 
-#default data
-useTestData = True
+# default data
+defaultUseTestData = True
+defaultSiteId = '06719505'  # Clear Creek at Golden
 defaultStartDate = datetime.date(1888, 1, 1)
-defaultEndDate = datetime.date(2020, 10, 25)
-defaultSiteId = '06719505' #Clear Creek at Golden
-defaultParameter = '00060' #cubic feet per second (cfs)
+defaultEndDate = datetime.date(2100, 12, 31)
+defaultParameter = '00060'  # cubic feet per second (cfs)
+defaultMinFlow = 300
+defaultMaxFlow = 1000
 
 defaultStatsDate = datetime.date(1000, 6, 4)
 
-app = Flask(__name__)
-@app.after_request
+# app = Flask(__name__)
+
+
+# @app.after_request
 def add_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     return response
 
-#test from console with exec(open('usgs-controller.py').read())
-@app.route('/')
-def getDailyAverageData(useTestData: bool = False,
-                        startDate: datetime.date = defaultStartDate, 
-                        endDate: datetime.date = defaultEndDate, 
-                        siteId: str = defaultSiteId, 
-                        gaugeParameter: str = defaultParameter) -> json:
+# test from console with exec(open('usgs-controller.py').read())
+
+
+# @app.route('/')
+def getUSGSDefaultData(event, object):
+    cleanData = cleanUSGSData(getUSGSData())
+    print('Data cleaned')
+    avg = cleanData.mean(axis=1)
+    return formatOutput(avg)
+
+
+# @app.route('/getDailyAverageData')
+def getDailyAverageData(event, object):
     '''Makes a usgs call with given or default parameters to create a clean dataframe object
+    '''
+    try:
+        siteId = event['queryStringParameters']['siteId']
+    except:
+        siteId =  defaultSiteId
+    
+    try:
+        startDate = event['queryStringParameters']['startDate']
+    except:
+        startDate = defaultStartDate # included for future ability to filter on years
+    
+    try:
+        endDate = event['queryStringParameters']['endDate']
+    except:
+        endDate = defaultEndDate # included for future ability to filter on years
+    
+    try:
+        gaugeParameter = event['queryStringParameters']['gaugeParameter']
+    except:
+        gaugeParameter = defaultParameter # included for future ability to use other params
+    
+    try:
+        testDataFlag = event['queryStringParameters']['testDataFlag']
+    except:
+        testDataFlag = False
+    
+    data = getUSGSData(testDataFlag, siteId, startDate,
+                       endDate, gaugeParameter)
+    cleanData = cleanUSGSData(data)
+    returnData = pd.DataFrame(cleanData.mean(axis=1), columns=['average'])
+    returnData['quantile20'] = cleanData.quantile(0.2, axis=1)
+    returnData['quantile80minus20'] = cleanData.quantile(
+        0.8, axis=1) - returnData['quantile20']
+    return formatOutput(returnData)
+
+
+# @ app.route('/getRunnablePercentages')
+def getDailyRunnablePercentage(event, object):
+    '''Takes in a mimimum and maximum value for the section and returns
+    a graph displaying the odds the section is runnable for each day
+    '''
+
+    try: 
+        siteId = event['queryStringParameters']['siteId']
+    except:
+        siteId = defaultSiteId
+  
+    try: 
+        minFlow = int(event['queryStringParameters']['minFlow'])
+    except:
+        minFlow = defaultMinFlow
+
+    try: 
+        maxFlow = int(event['queryStringParameters']['maxFlow'])
+    except:
+        maxFlow = defaultMaxFlow
+
+    try:
+        testDataFlag = event['queryStringParameters']['testDataFlag']
+    except:
+        testDataFlag = False
+
+    data = getUSGSData(testDataFlag, siteId)
+    averageData = cleanUSGSData(data)
+
+    countInRange = averageData[(averageData > minFlow) & (averageData < maxFlow)].count(axis=1)
+    totalCount = averageData[averageData > 0].count(axis=1)
+    dailyPercent = pd.DataFrame()
+    dailyPercent['percent'] = countInRange.div(totalCount)
+    # daysOver50 = percentages[percentages > 50]
+
+    return formatOutput(dailyPercent * 100)  
+
+def formatOutput(data, decimals: int = 0):
+    '''Creates json dictionary with value label on value objects
+    '''
+    response = {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': data.round(1).reset_index().to_json(orient='records')
+    }
+    
+    return response
+
+
+def getUSGSData(useTestData: bool = defaultUseTestData,
+                siteId: str = defaultSiteId,
+                startDate: datetime.date = defaultStartDate,
+                endDate: datetime.date = defaultEndDate,
+                gaugeParameter: str = defaultParameter) -> json:
+    '''Call USGS or get test data
     '''
 
     if useTestData:
         with open('testFile.json') as tf:
-            valueData = json.load(tf)
-    else:
-        url = f'https://waterservices.usgs.gov/nwis/dv/?format=json&startDT={startDate}&endDT={endDate}&parameterCd={gaugeParameter}&site={siteId}'
-        response = requests.get(url)
-        responseJson = response.json()
-        valueData = responseJson['value']['timeSeries'][0]['values'][0]['value'] #may have to change this if the json format is different
+            print('Using test data.')
+            return json.load(tf)
 
-    df = pd.DataFrame(valueData)
+    url = f'http://waterservices.usgs.gov/nwis/dv/?format=json&site={siteId}&startDT={startDate}&endDT={endDate}&parameterCd={gaugeParameter}'
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    responseJson = json.loads(response.data.decode('utf-8'))
+    # may have to change this if the json format is different
+    valueData = responseJson['value']['timeSeries'][0]['values'][0]['value']
+
+    return valueData
+
+
+def cleanUSGSData(jsonData):
+    '''Clean USGS Data
+    '''
+
+    df = pd.DataFrame(jsonData)
     df = df.drop('qualifiers', axis=1)
     df['value'] = df['value'].astype(float)
     df['dateTime'] = pd.to_datetime(df['dateTime'])
-    months, days, years = zip(*[(d.month, d.day, d.year) for d in df['dateTime']])
+    months, days, years = zip(*[(d.month, d.day, d.year)
+                                for d in df['dateTime']])
     df = df.assign(month=months, day=days, year=years)
     df = df.drop('dateTime', axis=1)
     df = df.pivot(index=['month', 'day'], columns='year', values='value')
-    df = df.T
-    
-    return df.to_json() 
-
-'''
-#input a df and a date
-@app.route('/getDailyStatistics', methods=['GET'])
-def getDailyStatistics(df: pd.DataFrame = pd.read_json(getDailyAverageData()), userDate: datetime.date = defaultStatsDate, minimumRunnable: int = 300) -> pd.Series:
-    ''Takes in a dataframe and a date to return the average flow, 
-    percent of years above the minimum, and the standard deviation flow
-    ''
-
-    dayMean = df[(userDate.month, userDate.day)].mean()
-    yearsRunnable = len(df[df[(userDate.month, userDate.day)] > minimumRunnable])
-    totalYears = len(df)
-    percentageOfYearsRunnable = 100 * yearsRunnable / totalYears
-    standardDeviation = df[(userDate.month, userDate.day)].std()
-
-    return pd.Series([dayMean, percentageOfYearsRunnable, standardDeviation], index=['mean', 'percentageOfYearsRunnable', 'standardDeviation'])
-
-    
-def getDailyRunnablePercentages(df: pd.DataFrame = pd.read_json(getDailyAverageData()), minimumRunnable: int = 300):
-    ''Takes in a dataframe and a minimum for the section and returns
-    a graph displaying the odds the section is runnable for each day
-    ''
-
-    f = lambda row: (row > minimumRunnable).mean()
-    percentages = df.apply(f)
-    daysOver50 = percentages[percentages > 50]
-    return percentages, daysOver50
-
-# # Basic chart
-# # df=pd.DataFrame({'x': range(1,101), 'y': np.random.randn(100)*15+range(1,101) })
-# fig, (runnableGraph, flowGraph) = plt.subplots(2, sharex=True)
-# plt.xlabel('Date')
-# plt.ylabel('Streamflow (cfs)')
-# runnableGraph.set_title('CC Golden Daily average cfs')
-# runnableGraph.plot(dates, runnableRate)
-# runnableGraph.set_ylabel('Percent of time runnable')
-# flowGraph.plot(dates, flow, marker='o', markersize=1)
-
-# plt.show()
-'''
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8888)
+    df.index = df.index.map(lambda t: f'{t[0]}/{t[1]}')
+    df[df <= 0] = np.nan
+    return df
