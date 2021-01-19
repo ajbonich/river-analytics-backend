3a07f1e1778c85a86297753dd5a3ef2e0e77c266
Andrew Bonich <ajbonich@gmail.com>
2021-01-19 09:45:16 -0700

    Updates

--- timeSeriesFlowcasting.py
+++ timeSeriesFlowcasting.py
@@ -1,7 +1,8 @@
-# AR example
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import acf, pacf
+from pandas.plotting import autocorrelation_plot
 from statsmodels.tsa.ar_model import AutoReg
 from random import random
-# contrived dataset
 
 import datetime
 import json
@@ -15,8 +16,6 @@ from math import sqrt
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
 import statsmodels.api as sm
 
-from statsmodels.tsa.seasonal import seasonal_decompose
-from dateutil.parser import parse
 
 train = pd.read_csv('trainData.csv')
 test = pd.read_csv('testData.csv')
@@ -27,17 +26,52 @@ test.set_index('dateTime', inplace=True)
 df = train.copy()
 df = df.asfreq('d')
 df = df.bfill()
-# Multiplicative Decomposition
-result_mul = seasonal_decompose(df['value'], model='multiplicative', extrapolate_trend='freq')
 
-# Additive Decomposition
-result_add = seasonal_decompose(df['value'], model='additive', extrapolate_trend='freq')
+# Showing seasonality
+'''
+plt.rcParams.update({'figure.figsize': (9, 5)})
+autocorrelation_plot(df.value.tolist())
+plt.show()
+'''
 
-# Plot
+# Calculate ACF and PACF upto 50 lags
+# acf_50 = acf(df.value, nlags=50)
+# pacf_50 = pacf(df.value, nlags=50)
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 3), dpi=100)
+plot_acf(df.value.tolist(), lags=50, ax=axes[0])
+plot_pacf(df.value.tolist(), lags=50, ax=axes[1])
+plt.show()
+
+# Analyzing stationary
+'''
+from statsmodels.tsa.stattools import adfuller
+result = adfuller(df.value.values, autolag='AIC')
+print(f'ADF Statistic: {result[0]}')
+print(f'p-value: {result[1]}')
+for key, value in result[4].items():
+    print('Critial Values:')
+    print(f'   {key}, {value}')
+'''
+
+# Decomposition
+'''
+from statsmodels.tsa.seasonal import seasonal_decompose
+result_mul = seasonal_decompose(
+    df['value'], model='multiplicative', extrapolate_trend='freq')
+result_add = seasonal_decompose(
+    df['value'], model='additive', extrapolate_trend='freq')
 plt.rcParams.update({'figure.figsize': (10, 10)})
 result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
 result_add.plot().suptitle('Additive Decompose', fontsize=22)
 plt.show()
+
+#Showing the decomp
+df_reconstructed = pd.concat(
+    [result_mul.seasonal, result_mul.trend, result_mul.resid, result_mul.observed], axis=1)
+df_reconstructed.columns = ['seas', 'trend', 'resid', 'actual_values']
+df_reconstructed.head()
+'''
 
 # train.value.plot(figsize=(15,8), title= 'Golden Flow History', fontsize=14)
 # test.value.plot(figsize=(15,8), title= 'Golden Flow History', fontsize=14)
