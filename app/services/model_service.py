1a8f0f32fc105fc0cad68b4e425f3fd80292c690
Andrew Bonich <ajbonich@gmail.com>
2021-02-23 14:58:47 -0700

    Changed adding method.

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -10,12 +10,12 @@ def generate_holt_winters_forecast(site_id: str, forecast_length: int) -> pd.Dat
     site_data = usgs_service.get_daily_average_data(site_id)
     clean_data = usgs_service.clean_USGS_data(site_data)
     offset_days = (dt.datetime.today() - clean_data.tail(1).index).days[0]
-    total_forecast_length = offset_days + forecast_length
+    offset_days += forecast_length
 
-    forecast_list = hwes.ets_forecast(site_data, total_forecast_length)
+    forecast_list = hwes.ets_forecast(site_data, offset_days)
     forecast_dates = pd.date_range(
         clean_data.tail(1).index[0] + dt.timedelta(days=1),
-        periods=total_forecast_length,
+        periods=offset_days,
     )
     formatted_dates = [date.strftime("%m/%d") for date in forecast_dates]
 
