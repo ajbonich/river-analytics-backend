70cb3f6f0587bf841993c097141d42e17f271737
Andrew Bonich <ajbonich@gmail.com>
2021-04-12 10:58:55 -0600

    Commented out print statements.

--- app/controllers/forecast_controller.py
+++ app/controllers/forecast_controller.py
@@ -28,8 +28,8 @@ def get_forecast(event: dict, object: object) -> dict:
     if model_type == "pystantest":
         import os
 
-        os.system("gcc --version")
-        os.system("which gcc")
+        # os.system("gcc --version")
+        # os.system("which gcc")
         test_result = "default_result"
         try:
             test_result = service.test_pystan()
