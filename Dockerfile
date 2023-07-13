c40a5fb2fa7353bc6a1b585f2ba130675fbebcd2
Andrew Bonich <ajbonich@gmail.com>
2023-07-13 15:30:36 -0600

    Adds root level lambda_function file with a handler function to try and fix aws import issues.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM public.ecr.aws/lambda/python:3.10
 
-COPY requirements.txt .
-RUN pip3 install --upgrade pip
-RUN pip3 install --user -r requirements.txt
-
 WORKDIR ${LAMBDA_TASK_ROOT}
+
+COPY requirements.txt ${LAMBDA_TASK_ROOT}
+
+COPY lambda_function.py ${LAMBDA_TASK_ROOT}
 
 COPY app ${LAMBDA_TASK_ROOT}/app
 
-CMD [ "app.controllers.forecast_controller.get_forecast" ]
+RUN pip3 install --upgrade pip
+RUN pip3 install -r requirements.txt
+
+CMD [ "lambda_function.handler" ]
+#CMD [ "app.controllers.forecast_controller.get_forecast" ]
--- app/controllers/forecast_controller.py
+++ app/controllers/forecast_controller.py
@@ -6,17 +6,20 @@ success_code = 200
 error_code = 400
 
 
-def get_forecast(event: dict, context: object) -> dict:
-    """Takes in an usgs site id, forecast model, and number of days and returns a list
-    of forecast values and 80% confidence interval for the given number of days.
-    Also returns the actual values before the current calendar day"""
+# def get_forecast(event: dict, context: object) -> dict:
+#     """Takes in an usgs site id, forecast model, and number of days and returns a list
+#     of forecast values and 80% confidence interval for the given number of days.
+#     Also returns the actual values before the current calendar day"""
+#
+#     try:
+#         site_id = event["site_id"]
+#         # model_type = event["pathParameters"]["modelType"]
+#         # number_of_days = event["queryStringParameters"]["days"]
+#     except Exception:
+#         return helper.format_output(error_code, "Bad inputs")
 
-    try:
-        site_id = event["site_id"]
-        # model_type = event["pathParameters"]["modelType"]
-        # number_of_days = event["queryStringParameters"]["days"]
-    except Exception:
-        return helper.format_output(error_code, "Bad inputs")
+
+def get_forecast(site_id: str) -> dict:
 
     # if model_type == "fbprophet":
     forecast = service.generate_prophet_forecast(site_id)
