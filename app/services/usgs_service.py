eeb86b84b4258ff68310ab8fb324cd4912365dca
Andrew Bonich <ajbonich@gmail.com>
2023-07-13 13:44:11 -0600

    Data cleaning fills in missing days with NaN and now keeps NaN values for UI to accurately show.

--- app/services/usgs_service.py
+++ app/services/usgs_service.py
@@ -7,6 +7,7 @@ import urllib3
 default_start_date = datetime.date(1888, 1, 1)
 default_end_date = datetime.date(2100, 12, 31)
 default_parameter = "00060"  # cubic feet per second (cfs)
+base_usgs_url = "http://waterservices.usgs.gov/nwis/dv/?format=json"
 
 
 def get_daily_average_data(
@@ -14,13 +15,14 @@ def get_daily_average_data(
     start_date: datetime.date = default_start_date,
     end_date: datetime.date = default_end_date,
     gauge_parameter: str = default_parameter,
-) -> pd.DataFrame:
+) -> dict:
     """Calls USGS api to get daily average values in the given date range"""
 
-    url = f"http://waterservices.usgs.gov/nwis/dv/?format=json&site={site_id}&startDT={start_date}&endDT={end_date}&parameterCd={gauge_parameter}"  # noqa: E501
+    url = f"{base_usgs_url}&site={site_id}&startDT={start_date}&endDT={end_date}&parameterCd={gauge_parameter}"  # noqa: E501
     http = urllib3.PoolManager()
     response = http.request("GET", url)
     response_json = json.loads(response.data.decode("utf-8"))
+    # may have to change this if the json format is different
     return response_json["value"]["timeSeries"][0]["values"][0]["value"]
 
 
@@ -31,16 +33,16 @@ def clean_data(json_data: dict) -> pd.DataFrame:
     df = df.drop("qualifiers", axis=1)
     df["value"] = df["value"].astype(float)
     df["dateTime"] = pd.to_datetime(df["dateTime"])
-    df[df["value"] <= 0] = np.nan
-    df = df.ffill()
-    df = df.bfill()
     df.set_index("dateTime", inplace=True)
-
+    df = df.asfreq("d")  # adds missing day data
+    df.loc[df["value"] <= 0, "value"] = np.nan
+    df = df.reset_index()
+    df = df[["dateTime", "value"]]
     return df
 
 
 def format_season_average_data(json_data: dict) -> pd.DataFrame:
-    """Given USGS data, cleans the data and formats to display seasonal averags"""
+    """Given USGS data, cleans the data and formats to display seasonal averages"""
 
     df = pd.DataFrame(json_data)
     df = df.drop("qualifiers", axis=1)
