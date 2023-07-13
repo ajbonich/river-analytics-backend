6cc7a4e9c329bdd6f60a4ada8d27f05af133af86
Andrew Bonich <ajbonich@gmail.com>
2023-07-13 13:50:14 -0600

    Added Prophet forecasting.

--- app/controllers/forecast_controller.py
+++ app/controllers/forecast_controller.py
@@ -1,31 +1,27 @@
 from app.helpers import controller_helper as helper
-
-# from app.services import model_service as service
+from app.services import model_service as service
 
 
 success_code = 200
-error_code = 401
+error_code = 400
 
 
-def get_forecast(event: dict, object: object) -> dict:
-    """Takes in a usgs site id, forecast model, and number of days and returns a list
-    of forecast values and 80% confidence interval for the given number of days"""
+def get_forecast(event: dict, context: object) -> dict:
+    """Takes in an usgs site id, forecast model, and number of days and returns a list
+    of forecast values and 80% confidence interval for the given number of days.
+    Also returns the actual values before the current calendar day"""
 
     try:
-        # site_id = event["pathParameters"]["site_id"]
-        model_type = event["pathParameters"]["modelType"]
+        site_id = event["site_id"]
+        # model_type = event["pathParameters"]["modelType"]
         # number_of_days = event["queryStringParameters"]["days"]
     except Exception:
-        return helper.format_output(400, "Bad inputs")
-
-    # if model_type == "holtwinters":
-    #     forecast = service.generate_holt_winters_forecast(site_id, number_of_days)
-    #     return helper.format_output(data=forecast)
+        return helper.format_output(error_code, "Bad inputs")
 
     # if model_type == "fbprophet":
-    #     forecast = service.generate_fbprophet_forecast(site_id, number_of_days)
-    #     return helper.format_output(success_code, data=forecast)
+    forecast = service.generate_prophet_forecast(site_id)
+    return helper.format_output(success_code, data=forecast)
 
-    return helper.format_output(
-        error_code, f"'{model_type}' is not a valid model type."
-    )
+    # return helper.format_output(
+    #     error_code, f"'{model_type}' is not a valid model type."
+    # )
